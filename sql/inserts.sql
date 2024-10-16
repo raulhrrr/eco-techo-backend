da539fa299cd2e6217ec461f573357728e5b557e
Raul Herrera <raulherrera01022003@gmail.com>
2024-10-15 20:28:20 -0500

    Refactor SQL queries and create tables for telemetry data and parameterization

--- sql/inserts.sql
+++ sql/inserts.sql
@@ -14,3 +14,21 @@ VALUES
     ('Humedad', 0, '%', 0, 100, 10, 90),
     ('Presión', 0, 'hPa', 0, 1000, 250, 950),
     ('Resistencia al gas', 0, 'kΩ', 0, 100, 20, 80);
+
+INSERT INTO
+    "tblUsers" (
+        email,
+        "name",
+        "password",
+        "isActive",
+        "createdAt",
+        "updatedAt"
+    )
+VALUES(
+    'user@mail.com',
+    'User',
+    '',
+    TRUE,
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP
+);
